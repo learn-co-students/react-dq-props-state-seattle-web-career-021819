4d6ab189f65fb20d6f11edb537a780829f8b5314
github username <github email address>
2019-04-28 10:52:24 -0700

    complete

--- src/MasterHog.js
+++ src/MasterHog.js
@@ -1,48 +1,51 @@
 import React, { Component } from 'react'
 import Master from './assets/master-hog.png'
 import BabyHog from './BabyHog'
-// import offspring from './db.js'
+import offspring from './db.js'
 
 export default class MasterHog extends Component {
 
   constructor() {
     super()
+    this.updateEyeColor=this.updateEyeColor.bind(this)
     this.state = {
       eyeColor: "blue",
+      offspring: offspring
     }
   }
 
+updateEyeColor(event){
+  let newColor = event.target.value
+  this.setState({eyeColor: newColor})
 
-  changeEyeColor = (e) => {
-    this.setState({
-      eyeColor: e.target.value
-    })
-  }
+}
+
 
 
   render() {
     return (
       <div>
 
-        <input type="radio" name="eyeColor" value="blue" onChange={this.changeEyeColor} />
+        <input type="radio" name="eyeColor" value="blue" onChange={this.updateEyeColor} />
         Blue<br></br>
-        <input type="radio" name="eyeColor" value="sun" onChange={this.changeEyeColor} />
+        <input type="radio" name="eyeColor" value="sun" onChange={this.updateEyeColor} />
         Sun<br></br>
-        <input type="radio" name="eyeColor" value="glowing" onChange={this.changeEyeColor} />
+        <input type="radio" name="eyeColor" value="glowing" onChange={this.updateEyeColor} />
         Glowing<br></br>
-      
+
         <h2>Name: Master Blaster</h2>
         <h3>Weight: 2.54 Tons</h3>
         <h3>Eye Color: {this.state.eyeColor}</h3>
-        
+
         <div id="masters-domicile">
           <img id="master-blaster" src={Master} alt="" />
         </div>
-        
+
         <ul className="hoglist">
-          <BabyHog />
-          <BabyHog />
-          <BabyHog />
+        {this.state.offspring.map((pig, index) => {
+          return <BabyHog key={index} hobby={pig.hobby} id={pig.id} name={pig.name} eyeColor={this.state.eyeColor}/>
+        })}
+
         </ul>
 
       </div>
